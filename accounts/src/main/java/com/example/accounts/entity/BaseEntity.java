e8939ddb800408a45305b055981cecc235b3df6e
nehasree25 <nehasreemendu@gmail.com>
2026-07-07 23:02:03 +0530

    add entity classes for Accounts and Customer, and create corresponding repositories

---
+++ accounts/src/main/java/com/example/accounts/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.accounts.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@MappedSuperclass
+@Getter @Setter @ToString
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false)
+    private LocalDate updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+
+}
---
+++ accounts/src/main/java/com/example/accounts/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.accounts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.accounts.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
